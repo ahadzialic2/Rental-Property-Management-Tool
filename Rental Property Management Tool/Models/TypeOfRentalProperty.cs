8749bb56aaf8592c8f726ff437e0ee5522a49abc
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-06-09 15:00:38 +0200

    models added

---
+++ Rental Property Management Tool/Models/TypeOfRentalProperty.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace Rental_Property_Management_Tool.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TypeOfRentalProperty
+    {
+        House,
+        Garage,
+        Apartment
+    }
+}
---
+++ Rental Property Management Tool/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Rental_Property_Management_Tool.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public List<RentalProperty> RentalProperties { get; set; }
+    }
+}
