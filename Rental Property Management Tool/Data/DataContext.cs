5d3d64801900a9b21030296f15abaed3fdd65df8
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-06-09 15:20:09 +0200

    added DataContext

---
+++ Rental Property Management Tool/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rental_Property_Management_Tool.Models;
+
+namespace Rental_Property_Management_Tool.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<RentalProperty> RentalProperties { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<OverheadCost> OverheadCosts { get; set; }
+    }
+}
