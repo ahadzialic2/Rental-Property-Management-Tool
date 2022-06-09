21f1160b58a55f52d789c67b22c377f1bbbf81d0
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-06-09 15:23:09 +0200

    added ServiceResponse

--- Rental Property Management Tool/Models/ServiceResponse.cs
+++ Rental Property Management Tool/Models/ServiceResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Rental_Property_Management_Tool.Models
 {
-    public class ServiceResponse
+    public class ServiceResponse<T>
     {
         public T Data { get; set; }
         public bool Success { get; set; } = true;
