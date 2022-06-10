ab5d9970e693d5bde10bc49a36365a9d654843d3
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-06-10 12:40:38 +0200

    added controller for RentalProperty

--- Rental Property Management Tool/Controllers/RentalPropertyController.cs
+++ Rental Property Management Tool/Controllers/RentalPropertyController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rental_Property_Management_Tool.Dtos.RentalProperty;
+using Rental_Property_Management_Tool.Models;
 using Rental_Property_Management_Tool.Services;
 
 namespace Rental_Property_Management_Tool.Controllers
@@ -9,7 +13,42 @@ namespace Rental_Property_Management_Tool.Controllers
         public RentalPropertyController(IRentalPropertyService rentalPropertyService)
         {
             _rentalPropertyService = rentalPropertyService; 
+        }
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<GetRentalPropertyDto>>>> Get()
+        {
+            return Ok(await _rentalPropertyService.GetAllRentalProperties());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetRentalPropertyDto>>> GetSingle(int id)
+        {
+            return Ok(await _rentalPropertyService.GetRentalPropertyById(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetRentalPropertyDto>>>> AddRentalProperty(AddRentalPropertyDto newRentalProperty)
+        {
+            return Ok(await _rentalPropertyService.AddRentalProperty(newRentalProperty));
+        }
 
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<GetRentalPropertyDto>>> UpdateRentalProperty(UpdateRentalPropertyDto updatedRentalProperty)
+        {
+            var response = await _rentalPropertyService.UpdateRentalProperty(updatedRentalProperty);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetRentalPropertyDto>>> Delete(int id)
+        {
+            var response = await _rentalPropertyService.DeleteRentalProperty(id);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
         }
     }
 }
