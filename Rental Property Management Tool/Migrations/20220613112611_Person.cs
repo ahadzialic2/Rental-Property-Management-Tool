b24669ecc2dbe97dc9b20ac63073f7b84c5ac30a
Adnan Hadzialic <ahadzialic2@etf.unsa.ba>
2022-06-13 13:30:47 +0200

    add flag property for soft deletion of RentalProperty and Person entities

---
+++ Rental Property Management Tool/Migrations/20220613112611_Person.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rental_Property_Management_Tool.Migrations
+{
+    public partial class Person : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "RentalProperties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Persons",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "RentalProperties");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Persons");
+        }
+    }
+}
