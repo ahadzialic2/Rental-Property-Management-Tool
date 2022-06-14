2964d1a29672ee8755f13a7d481a35dd209c497f
Alema <alemadu@maestralsolutions.com>
2022-06-14 15:16:48 +0200

    User login

---
+++ Rental Property Management Tool/Dtos/User/UserLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental_Property_Management_Tool.Dtos.User
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Rental Property Management Tool/Dtos/User/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Rental_Property_Management_Tool.Dtos.User
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
